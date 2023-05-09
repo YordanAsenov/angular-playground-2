b5d0c19cffa4a9172f6411927b504a4aa1146c6e
Yordan Asenov <yordan.asenov@intre.it>
2023-05-09 10:48:35 +0200

    🎨 adds ngZorro

--- pizza-maker/src/app/app.module.ts
+++ pizza-maker/src/app/app.module.ts
@@ -4,6 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { it_IT } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import it from '@angular/common/locales/it';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(it);
 
 @NgModule({
   declarations: [
@@ -12,9 +21,14 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: it_IT }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
