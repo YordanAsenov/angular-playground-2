a447e0276e7dc830b5aee7d48acc1166882fcf18
Yordan Asenov <yordan.asenov@intre.it>
2023-05-10 16:27:28 +0200

    ➕ adds ngrx-devtools v15.4.0

--- pizza-maker/src/app/app.module.ts
+++ pizza-maker/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +9,8 @@ import it from '@angular/common/locales/it';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzGridModule } from 'ng-zorro-antd/grid';
@@ -25,7 +27,6 @@ import { MenuComponent } from './menu/menu.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { PizzeListComponent } from './menu/pizze-list/pizze-list.component';
 import { PizzaDisplayComponent } from './menu/pizza-display/pizza-display.component';
-import { StoreModule } from '@ngrx/store';
 
 const ngModules = [
   NzLayoutModule,
@@ -54,8 +55,15 @@ registerLocaleData(it);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ...ngModules,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
+    ...ngModules
   ],
   providers: [
     { provide: NZ_I18N, useValue: it_IT }
